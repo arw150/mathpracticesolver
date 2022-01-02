721ce26e63729f26fabcad260c59e47edbed29ba
arw150 <68749219+arw150@users.noreply.github.com>
2022-01-02 13:53:44 -0500

    Update mathpracticesolver.py

--- mathpracticesolver.py
+++ mathpracticesolver.py
@@ -6,7 +6,8 @@ from sympy import symbols, sin, pi, pprint, latex, Limit, limit, Integral, oo, i
 import tkinter.font as font
 from tkinter import messagebox
 import random
-
+global loopstage
+loopstage = 0
 x_symbolic = symbols('x')
 sinc = sin(pi * x_symbolic) / (pi * x_symbolic)
 
@@ -15,10 +16,39 @@ def stringify_exponent(n, m):
     return f"{n}{superscripted}"
 def correctanswer():  
     messagebox.showinfo('Correct!', 'You got it right, on to the next step!')
-    loop = False
+    global loopstage
+    global questionbuttontext
     
+    loopstage = loopstage + 1
+    if (loopstage ==1):
+        Current_step['text'] = Problem1_Step2_question
+        button_correct['text'] = Problem1_Step2_First_Correct_Answer
+        button_false1['text'] = Problem1_Step2_First_Wrong_Answer
+        button_false2['text'] = Problem1_Step2_Second_Wrong_Answer
+    if (loopstage == 2):
+        Current_step['text'] = Problem1_Step3_question
+        button_correct['text'] = Problem1_Step3_First_Correct_Answer
+        button_false1['text'] = Problem1_Step3_First_Wrong_Answer
+        button_false2['text'] = Problem1_Step3_Second_Wrong_Answer
+    if (loopstage == 3):
+        Current_step['text'] = Problem_Final_Answer
+        button_correct['text'] = Problem1_Final_Answer_First_Correct_Answer
+        button_false1['text'] = Problem1_Final_Answer_First_Wrong_Answer
+        button_false2['text'] = Problem1_Final_Answer_Second_Wrong_Answer
+        Choosenext_textbox['text'] = gotitright
+        
+def getquestionbuttontext():
+    global loopstage
+    global questionbuttontext
+    if (loopstage == 0):
+        questionbuttontext = Problem1_Step1_question
+    if (loopstage == 1):
+        questionbuttontext = Problem1_Step2_question
+    #return questionbuttontext
 def wronganswer():
     messagebox.showinfo('Incoreect', 'Nope not right try again!')
+    global loopstage
+    loopstage = 0
 
     
     
@@ -66,27 +96,59 @@ _DIGITS = [
 ]
 
 #PROBLEM 1 DIFFERENT STEPS AND THEIR ANSWERS
-Problem1_question = '∫½x²'
-Problem1_First_Correct_Answer = '½'+'∫'+ str(stringify_exponent(x_symbolic,2))
-Problem1_First_Wrong_Answer = '∫'+ '½'+ str(stringify_exponent(x_symbolic,2))
-Problem1_Second_Wrong_Answer = '∫'+ '½' + str(stringify_exponent(x_symbolic,3))
+Problem1_Step1_question = '∫½x²'
+Problem1_Step1_First_Correct_Answer = '½'+'∫'+ str(stringify_exponent(x_symbolic,2))
+Problem1_Step1_First_Wrong_Answer = '∫'+ '½'+ str(stringify_exponent(x_symbolic,2))
+Problem1_Step1_Second_Wrong_Answer = '∫'+ '½' + str(stringify_exponent(x_symbolic,3))
+Problem1_Step2_question = '½'+'∫'+ str(stringify_exponent(x_symbolic,2))
+Problem1_Step2_First_Correct_Answer = '½ * ⅓' + str(stringify_exponent(x_symbolic,3))
+Problem1_Step2_First_Wrong_Answer = '½ / ⅓' + str(stringify_exponent(x_symbolic,3))
+Problem1_Step2_Second_Wrong_Answer = '½ * ⅓' + str(stringify_exponent(x_symbolic,4))
+Problem1_Step3_question = '½ * ⅓' + str(stringify_exponent(x_symbolic,3))
+Problem1_Step3_First_Correct_Answer = '⅙' + str(stringify_exponent(x_symbolic,3))
+Problem1_Step3_First_Wrong_Answer = '⅙' + str(stringify_exponent(x_symbolic,4))
+Problem1_Step3_Second_Wrong_Answer = '⅖' + str(stringify_exponent(x_symbolic,3))
+Problem_Final_Answer = '∫½x² = ' + '⅙' + str(stringify_exponent(x_symbolic,3))
+Problem1_Final_Answer_First_Correct_Answer = ''
+Problem1_Final_Answer_First_Wrong_Answer = ''
+Problem1_Final_Answer_Second_Wrong_Answer = ''
+gotitright = 'You got it right!'
 
-
-
-#OPENS THE WINDOW, DISPLAYS THE BUTTONS,SETS THEIR COMMANDS AND CALL BACKS, A LOOP WILL BE PUT IN TELLING IT TO GO AGAIN 
-#WITH THE NEXT STAGE OF THE PROBLEM WHEN A CORRECT ANSWER IS CHOSEN
 
 window = tkinter.Tk()
 window.title("Math Practice Solver")
-window.geometry("1000x750")
-Current_step = tkinter.Button(window,text= '∫½x²')
+window.geometry("1250x750")
+Current_step = tkinter.Button(window,text= Problem1_Step1_question)
 Current_step['font'] = font.Font(size=50)
-Choosenext_textbox = tkinter.Button(window,text='Pick the correct next step in solving this problem')
-Choosenext_textbox['font'] = font.Font(size=25)
+Current_step.pack()
+Current_step.place(x=400,y=0)
 
+Choosenext_textbox = tkinter.Button(window,text='Pick the correct next step in solving this problem')
+Choosenext_textbox.pack_forget()
+Choosenext_textbox['font'] = font.Font(size=25)
+Choosenext_textbox.pack()
+Choosenext_textbox.place(x=200,y=150)
+#OPENS THE WINDOW, DISPLAYS THE BUTTONS,SETS THEIR COMMANDS AND CALL BACKS, A LOOP WILL BE PUT IN TELLING IT TO GO AGAIN 
+#WITH THE NEXT STAGE OF THE PROBLEM WHEN A CORRECT ANSWER IS CHOSEN
+#while (loopstage <=3):
+   
+   
+    
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
 #THE CORRECT BUTTON
-button_correct_text = Problem1_First_Correct_Answer
+button_correct_text = Problem1_Step1_First_Correct_Answer
 button_correct = tkinter.Button(window,text= button_correct_text, command=correctanswer)
+button_correct.pack_forget()
 button_correct['font'] = font.Font(size=50)
 button_correct.pack()  
 button_correct.place(x=button_correct_xcoord,y=300)
@@ -94,24 +156,27 @@ print(button_correct_xcoord)
 #button_correct.grid(row = 10, column = 0)
 
 #THE FIRST INCORRECT PROBLEM
-button_false1_text = Problem1_First_Wrong_Answer
+button_false1_text = Problem1_Step1_First_Wrong_Answer
 button_false1 = tkinter.Button(window,text= button_false1_text, command = wronganswer)
+button_false1.pack_forget()
 button_false1['font'] = font.Font(size=50)
 button_false1.pack()
-button_false1.place(x=(button_false1_xcoord-40),y=300)
+button_false1.place(x=(button_false1_xcoord),y=300)
 print(button_false1_xcoord)
 
 #THE SECOND INCORRECT PROBLEM
-button_false2_text = Problem1_Second_Wrong_Answer
+button_false2_text = Problem1_Step1_First_Wrong_Answer
 button_false2 = tkinter.Button(window,text= button_false2_text, command = wronganswer)
+button_false2.pack_forget()
 button_false2['font'] = font.Font(size=50)
 button_false2.pack()
 button_false2.place(x=button_false2_xcoord, y=300)
 print(button_false2_xcoord)
+window.update()
+if (loopstage == 3):
+    window.quit()
 
-Current_step.pack()
-Choosenext_textbox.pack()
-
+print(loopstage)
 
 #if()
 tkinter.mainloop()
